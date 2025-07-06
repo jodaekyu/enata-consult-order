dac018f24124fd8733fd8de72c1525a6cf0a7099
jodaekyu <jodacu001@gmail.com>
2025-07-06 20:20:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -80,7 +80,10 @@ onAuthStateChanged(auth, (user) => {
     if (email === "kara@email.com") currentUserName = "카라";
     if (email === "kirke@email.com") currentUserName = "키르케";
 
-    document.getElementById("loginStatus").innerText = `${currentUserName}님 로그인됨`;
+ document.getElementById("loginStatus").innerText = "로그인 성공!";
+    document.getElementById("loginBox").style.display = "none";      // 로그인칸 숨김
+    document.getElementById("signupBox").style.display = "none";     // 회원가입칸 숨김
+    document.getElementById("userEmailDisplay").innerText = `${email} 로그인중`;  // 오른쪽 상단 표시
   }
 });
 
