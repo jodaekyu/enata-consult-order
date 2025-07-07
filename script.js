15258ca46befe44792a964af3665d7645dcf0246
jodaekyu <jodacu001@gmail.com>
2025-07-08 02:17:08 +0900

    Update script.js

--- script.js
+++ script.js
@@ -545,7 +545,6 @@ document.addEventListener("DOMContentLoaded", () => {
   populateDateDropdowns();
 });
 // Firebase SDK 모듈 임포트
-import { signOut } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import {
   getAuth,
